7797390ca4d292605b0ae2723e075b7949a13f97
Max de Groot <max@maximumfx.nl>
2025-07-11 09:57:21 +0200

    Rewrite app to add support for sequences

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 File Cache node App for use with Toolkit's Houdini engine.
 """
 
+import hou
 import sgtk
+
 
 class TkCacheNodeApp(sgtk.platform.Application):
     def init_app(self):
@@ -10,6 +12,12 @@ class TkCacheNodeApp(sgtk.platform.Application):
 
         tk_houdini_cachenode = self.import_module("tk_houdini_cachenode")
         self.handler = tk_houdini_cachenode.TkCacheNodeHandler(self)
+
+    def setup_node(self, node: hou.Node):
+        """
+        Set up the supplied node on creation.
+        """
+        return self.handler.setup_node(node)
 
     def get_nodes(self):
         """
@@ -22,12 +30,11 @@ class TkCacheNodeApp(sgtk.platform.Application):
         """
 
         self.log_debug("Retrieving tk-houdini-cache nodes...")
-        tk_houdini_cache = self.import_module("tk_houdini_cachenode")
-        nodes = tk_houdini_cache.TkCacheNodeHandler.get_nodes()
+        nodes = self.handler.get_nodes()
         self.log_debug("Found %s tk-houdini-cache nodes." % (len(nodes),))
         return nodes
 
-    def get_output_path(self, node):
+    def get_output_path(self, node: hou.Node):
         """
         Returns the evaluated output path for the supplied node.
         Example usage::
@@ -38,21 +45,61 @@ class TkCacheNodeApp(sgtk.platform.Application):
         """
 
         self.log_debug("Retrieving output path for %s" % (node,))
-        tk_houdini_cache = self.import_module("tk_houdini_cachenode")
-        output_path = tk_houdini_cache.TkCacheNodeHandler.get_output_path(node)
+        output_path = self.handler.get_output_path(node)
         self.log_debug("Retrieved output path: %s" % (output_path,))
         return output_path
+
+    def refresh_output_path(self, node: hou.Node):
+        """
+        Refreshes the output path for the supplied node.
+        """
+        return self.handler.refresh_output_path(node)
+
+    def get_output_range(self, node: hou.Node) -> list[int]:
+        """Get output frame range for the supplied node
+
+        Args:
+            node (hou.Node): SGTK Cache node
+        """
+        return self.handler.get_output_range(node)
 
     def get_work_file_template(self):
         """
         Returns the configured work file template for the app.
         """
 
-        return self.get_template("output_cache_template")
+        return self.get_template("work_file_template")
 
-    def get_publish_file_template(self):
+    def get_node_render_template(self, node: hou.Node):
+        """
+        Returns the configured cache file template for the app.
+
+        Args:
+            node (hou.Node): SGTK Cache node
+        """
+
+        time_dependent = node.parm("timedependent").eval()
+
+        # Get the cache templates from the app
+        return (
+            self.get_template("output_cache_sequence_template")
+            if time_dependent
+            else self.get_template("output_cache_template")
+        )
+
+    def get_node_publish_template(self, node: hou.Node):
         """
         Returns the configured publish file template for the app.
+
+        Args:
+            node (hou.Node): SGTK Cache node
         """
 
-        return self.get_template("output_publish_template")
+        time_dependent = node.parm("timedependent").eval()
+
+        # Get the cache templates from the app
+        return (
+            self.get_template("output_publish_sequence_template")
+            if time_dependent
+            else self.get_template("output_publish_template")
+        )
--- python/tk_houdini_cachenode/handler.py
+++ python/tk_houdini_cachenode/handler.py
@@ -1,31 +1,25 @@
-import sgtk
-import hou
 import os
-import re
+
+import hou
+import sgtk
 
 
 class TkCacheNodeHandler(object):
-
     TK_CACHE_NODE_TYPE = "sgtk_cache"
-    NODE_OUTPUT_PATH_PARM = "pathString"
+    NODE_OUTPUT_PATH_PARM = "file"
 
     def __init__(self, app):
         self._app = app
 
-    def setupNode(self, node):
-
-        # get parameters and computed output path
-        path = node.parm("path")
-        pathString = node.parm("pathString")
-        outputPath = self._computeOutputPath(node)
-        outputLabel = os.path.split(outputPath)[1]
+    def setup_node(self, node):
+        default_name = self._app.get_setting("default_node_name")
+        node.setName(default_name, unique_name=True)
 
         try:
-            pathString.set(outputPath)
-            path.set(outputLabel)
+            self._app.log_metric("Create", log_version=True)
         except:
-            e = "The output path could not be calculated!"
-            raise sgtk.TankError(e)
+            # ingore any errors. ex: metrics logging not supported
+            pass
 
     @classmethod
     def get_nodes(cls):
@@ -34,17 +28,10 @@ class TkCacheNodeHandler(object):
         session.
         """
 
-        tk_node_type = TkCacheNodeHandler.TK_CACHE_NODE_TYPE
-
         # get all instances of tk cache sop nodes
-
-        tk_cache_nodes = []
-
-        tk_cache_nodes.extend(
-            hou.nodeType(hou.sopNodeTypeCategory(), tk_node_type).instances()
-        )
-
-        return tk_cache_nodes
+        return hou.nodeType(
+            hou.sopNodeTypeCategory(), TkCacheNodeHandler.TK_CACHE_NODE_TYPE
+        ).instances()
 
     @classmethod
     def get_output_path(cls, node):
@@ -53,8 +40,33 @@ class TkCacheNodeHandler(object):
         """
 
         output_parm = node.parm(cls.NODE_OUTPUT_PATH_PARM)
-        path = output_parm.eval()
+        path = output_parm.menuLabels()[output_parm.eval()]
         return path
+
+    # refresh the output profile path
+    def refresh_output_path(self, node):
+        output_path_parm = node.parm(self.NODE_OUTPUT_PATH_PARM)
+        output_path_parm.set(output_path_parm.eval())
+
+    @classmethod
+    def get_output_range(cls, node: hou.Node) -> list[int]:
+        """This function returns our frame range or a single frame."""
+        time_dependent = node.parm("timedependent").eval()
+        framerange_type = node.parm("trange").eval()
+
+        if time_dependent:
+            if framerange_type > 0:
+                start_frame = int(node.parm("f1").eval())
+                end_frame = int(node.parm("f2").eval())
+                framerange = [start_frame, end_frame]
+            else:
+                current_frame = int(hou.frame())
+                framerange = [current_frame, current_frame]
+        else:
+            current_frame = int(node.parm("static_frame").eval())
+            framerange = [current_frame, current_frame]
+
+        return framerange
 
     # private methods
 
@@ -76,26 +88,38 @@ class TkCacheNodeHandler(object):
         # get relevant fields from the current file path
         work_file_fields = self._getHipfileFields()
 
-        # get name attribute from node
-        parm = node.parm("description")
-        name = parm.eval()
-
         if not work_file_fields:
             msg = "This Houdini file is not a Shotgun Toolkit work file! Save the file through Shotgun save and create the node again."
             hou.ui.displayMessage(msg, buttons=("OK",), severity=hou.severityType.Error)
             raise sgtk.TankError(msg)
 
+        time_dependent = node.parm("timedependent").eval()
+
         # Get the cache templates from the app
-        output_cache_template = self._app.get_template("output_cache_template")
-
-        work_file_fields["name"] = name
-
-        # update those fields with the output template
-        work_file_fields.update(
-            self._app.context.as_template_fields(output_cache_template)
+        output_cache_template = (
+            self._app.get_template("output_cache_sequence_template")
+            if time_dependent
+            else self._app.get_template("output_cache_template")
         )
 
-        path = output_cache_template.apply_fields(work_file_fields)
+        # create fields dict with all the metadata
+        fields = {
+            "name": work_file_fields.get("name", None),
+            "node": node.parm("name").eval(),
+            "cache_version": node.parm("version").eval(),
+            "version": node.parm("version").eval(),
+        }
+
+        if time_dependent:
+            fields["SEQ"] = "FORMAT: $F"
+
+        if node.parm("use_sg_version").eval():
+            fields["version"] = work_file_fields.get("name", None)
+
+        # update those fields with the output template
+        fields.update(self._app.context.as_template_fields(output_cache_template))
+
+        path = output_cache_template.apply_fields(fields)
         path = path.replace(os.path.sep, "/")
 
         return path
